65266acc4f625f666ee4c2f004d4844a2383d95d
georgidelchev <alienguymc@gmail.com>
2020-04-23 11:55:52 +0300

    add files via upload

---
+++ 002 - [Basic Syntax, Conditional Statements and Loops - Exercise]/005 - [Login]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+            string passwordInsert = Console.ReadLine();
+            int counter = 0;
+            while (passwordInsert != password)
+            {
+
+                counter++;
+                if (counter == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                passwordInsert = Console.ReadLine();
+            }
+            if (passwordInsert == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
