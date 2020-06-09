31b734e40722bff48bc37516e443ab7d033abbfb
georgidelchev <alienguymc@gmail.com>
2020-06-10 00:19:16 +0300

    add files via upload

---
+++ [Programming Fundamentals Exercises - Archive]/01 - [C# Intro and Basic Syntax - Lab]/02 - [Add Two Numbers]/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+        }
+    }
+}
