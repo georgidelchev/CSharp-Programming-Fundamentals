c2e3869384e94d116f6d16802739654209d4b665
georgidelchev <alienguymc@gmail.com>
2020-06-10 00:19:35 +0300

    add files via upload

---
+++ [Programming Fundamentals Exercises - Archive]/01 - [C# Intro and Basic Syntax - Lab]/03 - [Employee Data]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int id = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}\n" +
+                              $"Age: {age}\n" +
+                              $"Employee ID: {id:d8}\n" +
+                              $"Salary: {salary:f2}");
+        }
+    }
+}
